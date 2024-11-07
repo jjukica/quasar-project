6644e47d123cd5f78ec3205cbc9f67ffe45cdf7e
Juraj <jjukica@veleri.hr>
2024-11-07 11:17:10 +0100

    O nama

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,11 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+<q-page class="flex flex-center">
+  <img
+    alt="Knjiznica slika"
+    src="~assets/library.jpg"
+  >
+</q-page>
+
 </template>
 
 <script setup>
