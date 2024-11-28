4f257dca7c8b36c55b4c67c6da07763d1327049a
Juraj <jjukica@veleri.hr>
2024-11-28 12:50:37 +0100

    Dodavanje metoda za get u backend

--- Backend/indeks.js
+++ Backend/indeks.js
@@ -26,6 +26,36 @@ connection.connect(function (err) {
     console.log("Connected!");
 });
 
+app.get("/api/knjige", (req, res) => {
+    connection.query("SELECT * FROM knjiga", (error, results) => {
+        if (error) throw error;
+        res.send(results);
+    });
+})
+app.get("/api/knjige/:id", (req, res) => {
+    const id = req.params.id;
+
+    res.send("jedna knjiga " + id);
+})
+
+app.get("/api/knjige/:autor", (req, res) => {
+    connection.query("SELECT * FROM knjiga WHERE autor like %autor%", (error, results) => {
+        if (error) throw error;
+        res.send(results);
+    });
+})
+
+
+app.post("/api/rezerv_knjige", (req, res) => {
+    const data = req.body;
+    rezervacija = [[data.datum, data.id_knjiga, data.id_korisnik]]
+    connection.query("INSERT INTO rezervacija (datum_rez, knjiga, korisnik) VALUES ?", [rezervacija], (error, results) => {
+        if (error) throw error;
+        res.send(results);
+    });
+});
+
+
 app.listen(port, () => {
     console.log("Server running at port: " + port);
 });
